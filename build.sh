afff3e9f6cc859e5a9060f9400597f2e2eeb4cfb
Brian Tierney <bltierney@utexas.edu>
2023-08-03 14:41:30 -0500

    removed --squash, as its not supported everywhere

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 VERSION=${1:-1.6.18}
 
-docker build --build-arg NFDUMP_VERSION=$VERSION  --tag netsage/nfdump-collector:$VERSION --squash  collector
-docker build --build-arg NFDUMP_VERSION=$VERSION  --tag netsage/nfdump-collector:alpine-$VERSION -f collector/Dockerfile-alpine --squash  .
+docker build --build-arg NFDUMP_VERSION=$VERSION  --tag netsage/nfdump-collector:$VERSION  collector
+#docker build --build-arg NFDUMP_VERSION=$VERSION  --tag netsage/nfdump-collector:$VERSION --squash  collector
+docker build --build-arg NFDUMP_VERSION=$VERSION  --tag netsage/nfdump-collector:alpine-$VERSION -f collector/Dockerfile-alpine  .
+#docker build --build-arg NFDUMP_VERSION=$VERSION  --tag netsage/nfdump-collector:alpine-$VERSION -f collector/Dockerfile-alpine --squash  .
